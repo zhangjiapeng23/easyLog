c47961d13a93036d4daa694eb91c503d18d41109
张佳鹏 <18373230129@163.com>
2022-08-21 21:17:35 +0800

    1、Fixed log filter will null pointer error 2、update log entity datetime format

--- easyLog.go
+++ easyLog.go
@@ -10,15 +10,5 @@ func main() {
 	for {
 		menu.CurrentMenu.ShowMenu()	
 	}
-	// k8s.Tmp()
-	// fmt.Println(k8s.NewClient("pre").ListNamespaces())
-	// k := k8s.NewClient("pre")
-	// k.ListAppsForNamespace("office")
-	// fmt.Print("end")
-	// res := k.ListPodsForApp("office", "webull-auth-center")
-	// for _, pod := range res.Items {
-	// 	fmt.Println(pod.Name)
-	// }
-
 }
 
--- filters/logFilters.go
+++ filters/logFilters.go
@@ -4,66 +4,26 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func ErrorFilter(log chan []byte, filterLog chan *Log, extra ...string) {
-	lineStartPattern, _ := regexp.Compile("^[0-9]+-[0-9]+-[0-9]+.*")
-	errorLogPattern, _ := regexp.Compile(".*(ERROR).*")
-	dateTimePattern, _ := regexp.Compile("[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+,[0-9]+")
-	titleTimePattern, _ := regexp.Compile(`.*?\[.*?\](.*)`)
-	var logEntity *Log
-	for {
-		logMsg, ok := <-log
-		if ok {
-			if lineStartPattern.Match(logMsg) && errorLogPattern.Match(logMsg) {
-				level := errorLogPattern.FindSubmatch(logMsg)[1]
-				dateTimeStr := dateTimePattern.FindSubmatch(logMsg)[0]
-				title := strings.Trim(string(titleTimePattern.FindSubmatch(logMsg)[1]), " ")
-				logEntity = NewLog(string(dateTimeStr), string(level), title)
-			} else if !lineStartPattern.Match(logMsg) && logEntity != nil {
-				logEntity.AddErrorLog(logMsg)
-			} else if logEntity != nil {
-				filterLog <- logEntity
-				logEntity = nil
-			}
-		} else {
-			break
-		}
+	levelFilter("ERROR", log, filterLog, extra...)
+}
 
-	}
+func WarnFilter(log chan []byte, filterLog chan *Log, extra ...string) {
+	levelFilter("WARN", log, filterLog, extra...)
 }
 
 func InfoFilter(log chan []byte, filterLog chan *Log, extra ...string) {
-	lineStartPattern, _ := regexp.Compile("^[0-9]+-[0-9]+-[0-9]+.*")
-	infoLogPattern, _ := regexp.Compile(".*(INFO).*")
-	dateTimePattern, _ := regexp.Compile("[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+,[0-9]+")
-	titleTimePattern, _ := regexp.Compile(`.*?\[.*?\](.*)`)
-	var logEntity *Log
-	for {
-		logMsg, ok := <-log
-		if ok {
-			if lineStartPattern.Match(logMsg) && infoLogPattern.Match(logMsg) {
-				level := infoLogPattern.FindSubmatch(logMsg)[1]
-				dateTimeStr := dateTimePattern.FindSubmatch(logMsg)[0]
-				title := strings.Trim(string(titleTimePattern.FindSubmatch(logMsg)[1]), " ")
-				logEntity = NewLog(string(dateTimeStr), string(level), title)
-			} else if !lineStartPattern.Match(logMsg) && logEntity != nil {
-				logEntity.AddErrorLog(logMsg)
-			} else if logEntity != nil {
-				filterLog <- logEntity
-				logEntity = nil
-			}
-		} else {
-			break
-		}
-	}
+	levelFilter("INFO", log, filterLog, extra...)
 }
 
 func KeywordFilter(log chan []byte, filterLog chan *Log, extra ...string) {
-	lineStartPattern, _ := regexp.Compile("^[0-9]+-[0-9]+-[0-9]+.*")
+	lineStartPattern, _ := regexp.Compile("[0-9]+-[0-9]+-[0-9]+.*")
 	levelLogPattern, _ := regexp.Compile(".*(INFO|ERROR).*")
-	dateTimePattern, _ := regexp.Compile("[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+,[0-9]+")
-	titleTimePattern, _ := regexp.Compile(`.*?\[.*?\](.*)`)
+	dateTimePattern, _ := regexp.Compile("[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+")
+	titlePattern, _ := regexp.Compile(`.*?\[.*?\](.*)`)
 	keyword := ""
 	if len(extra) > 0 {
 		keyword = extra[0]
@@ -77,24 +37,33 @@ func KeywordFilter(log chan []byte, filterLog chan *Log, extra ...string) {
 			if lineStartPattern.Match(logMsg) && levelLogPattern.Match(logMsg) {
 				level := levelLogPattern.FindSubmatch(logMsg)[1]
 				dateTimeStr := dateTimePattern.FindSubmatch(logMsg)[0]
-				title := strings.Trim(string(titleTimePattern.FindSubmatch(logMsg)[1]), " ")
-				if logEntity != nil && logEntity.IsMatch {
-					filterLog <- logEntity
+				titleMatch := titlePattern.FindSubmatch(logMsg)
+				title := ""
+				if len(titleMatch) > 0 {
+					title = strings.Trim(string(titleMatch[1]), " ")
+				} else {
+					title = strings.Trim(string(logMsg), " ")
 				}
 				logEntity = NewLog(string(dateTimeStr), string(level), title)
 				if keywordPattern.Match(logMsg) {
 					logEntity.IsMatch = true
+					go func(log *Log) {
+						time.AfterFunc(1*time.Second, func() {
+							filterLog <- log
+						})
+					}(logEntity)
 				}
 			} else if !lineStartPattern.Match(logMsg) && logEntity != nil {
-				logEntity.AddErrorLog(logMsg)
-				if keywordPattern.Match(logMsg) {
+				logEntity.AddLogDetail(logMsg)
+				// if log detial match keyword and log is not ready to print
+				if keywordPattern.Match(logMsg) && !logEntity.IsMatch {
 					logEntity.IsMatch = true
+					go func(log *Log) {
+						time.AfterFunc(1*time.Second, func() {
+							filterLog <- log
+						})
+					}(logEntity)
 				}
-			} else if logEntity != nil {
-				if logEntity.IsMatch {
-					filterLog <- logEntity
-				}
-				logEntity = nil
 			}
 		} else {
 			break
@@ -110,5 +79,41 @@ func AllFilter(log chan []byte, filterLog chan *Log, extra ...string) {
 		} else {
 			break
 		}
+	}
+}
+
+func levelFilter(level string, log chan []byte, filterLog chan *Log, extra ...string) {
+	levelPatterString := fmt.Sprintf(".*(%s).*", level)
+	lineStartPattern, _ := regexp.Compile("[0-9]+-[0-9]+-[0-9]+.*")
+	levelLogPattern, _ := regexp.Compile(levelPatterString)
+	dateTimePattern, _ := regexp.Compile("[0-9]+-[0-9]+-[0-9]+ [0-9]+:[0-9]+:[0-9]+")
+	titlePattern, _ := regexp.Compile(`.*?\[.*?\](.*)`)
+	var logEntity *Log
+	for {
+		logMsg, ok := <-log
+		if ok {
+			if lineStartPattern.Match(logMsg) && levelLogPattern.Match(logMsg) {
+				level := levelLogPattern.FindSubmatch(logMsg)[1]
+				dateTimeStr := dateTimePattern.FindSubmatch(logMsg)[0]
+				titleMatch := titlePattern.FindSubmatch(logMsg)
+				title := ""
+				if len(titleMatch) > 0 {
+					title = strings.Trim(string(titleMatch[1]), " ")
+				} else {
+					title = strings.Trim(string(logMsg), " ")
+				}
+				logEntity = NewLog(string(dateTimeStr), string(level), title)
+				go func(log *Log) {
+					time.AfterFunc(1*time.Second, func() {
+						filterLog <- log
+					})
+				}(logEntity)
+			} else if !lineStartPattern.Match(logMsg) && logEntity != nil {
+				logEntity.AddLogDetail(logMsg)
+			}
+		} else {
+			break
+		}
+
 	}
 }
